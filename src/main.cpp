61185cc6a367e0f43be0068d8099a5830e5f0974
ckcr4lyf <poiasdpoiasd@live.com>
2023-05-04 09:41:52 +0800

    Base mp3 player

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
 
 #include "AudioFileSourcePROGMEM.h"
+#include "AudioFileSourceID3.h"
+#include "AudioGeneratorMP3.h"
 #include "AudioGeneratorWAV.h"
 #include "AudioOutputI2S.h"
 
 // VIOLA sample taken from https://ccrma.stanford.edu/~jos/pasp/Sound_Examples.html
 // #include "viola.h"
-// #include "fly_64k.h"
+#include "fly_64k.h"
 // #include "cantina.h"
-#include "fly_short.h"
+// #include "fly_short.h"
 
-AudioGeneratorWAV *wav;
+AudioGeneratorMP3 *mp3;
 AudioFileSourcePROGMEM *file;
 AudioOutputI2S *out;
+AudioFileSourceID3 *id3;
+
+// Called when a metadata event occurs (i.e. an ID3 tag, an ICY block, etc.
+void MDCallback(void *cbData, const char *type, bool isUnicode, const char *string)
+{
+  (void)cbData;
+  Serial.printf("ID3 callback for: %s = '", type);
+
+  if (isUnicode) {
+    string += 2;
+  }
+  
+  while (*string) {
+    char a = *(string++);
+    if (isUnicode) {
+      string++;
+    }
+    Serial.printf("%c", a);
+  }
+  Serial.printf("'\n");
+  Serial.flush();
+}
 
 void setup()
 {
@@ -23,19 +47,21 @@ void setup()
   audioLogger = &Serial;
   // file = new AudioFileSourcePROGMEM(viola, sizeof(viola));
   file = new AudioFileSourcePROGMEM(rawData, sizeof(rawData));
+  id3 = new AudioFileSourceID3(file);
+  id3->RegisterMetadataCB(MDCallback, (void*)"ID3TAG");
   out = new AudioOutputI2S();
   out->SetPinout(4, 5, 18);
   // out->SetRate(44100);
-  wav = new AudioGeneratorWAV();
-  wav->begin(file, out);
+  mp3 = new AudioGeneratorMP3();
+  mp3->begin(id3, out);
 }
 
 void loop()
 {
-  if (wav->isRunning()) {
-    if (!wav->loop()) wav->stop();
+  if (mp3->isRunning()) {
+    if (!mp3->loop()) mp3->stop();
   } else {
-    Serial.printf("WAV done\n");
+    Serial.printf("MP3 done\n");
     delay(1000);
   }
 }
